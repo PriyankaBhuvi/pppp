d49e62df470fe45fdf69f2d59e8e34c2aababf1c
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-19 21:26:53 +0530

    Add files via upload

---
+++ SwitchStatement.java
@@ -0,0 +1,57 @@
+package com.vstl.javatraining;
+
+public class SwitchStatement {
+	
+	
+     static int intday =1;
+	    
+	
+	  public void daysInWeek (int intday) {
+		     
+		 
+		 switch(intday)  {
+		 
+		 
+		  
+		 case 1 :         
+		       System.out.println("1st day is Sunday");
+		        break;
+		        
+		        
+		 case 2 :
+			 System.out.println("2nd day is Monday");
+		       break;
+		      
+		       
+		 case 3:
+		 
+		     System.out.println("3rd day is Tuesday");
+		       break;
+		       
+		       
+		 case  4:
+		      System.out.println("4th day is  Wednesday");
+		      break;
+		      
+		      
+		 case 5:
+			 System.out.println("5th day is Thursday");
+			 
+			 
+		 case 6:
+		    System.out.println("6th day is Friday");
+		    
+		    
+		 case  7:
+			 System.out.println("7th day is Saturday");
+			 
+		 }			 
+
+}
+	  
+	  
+	  
+}	  
+	  
+	  
+	  
