92102659f7571f3553c7677d40b5d23420d96572
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-19 21:30:00 +0530

    Add files via upload

---
+++ JavaTest.java
@@ -0,0 +1,13 @@
+package com.accessModifiers;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		
+		JavaAccessSpecifires objJavaAccessSpecifires = new JavaAccessSpecifires();
+		 objJavaAccessSpecifires.strStudentsResult();
+
+   
+	}
+
+}
