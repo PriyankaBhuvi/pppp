61406ea1b0270dcf8a9eb208a66787c5c0a3b015
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-19 21:26:25 +0530

    Add files via upload

---
+++ TestIfElseControlStatement.java
@@ -0,0 +1,29 @@
+package com.vstl.javatraining;
+
+public class TestIfElseControlStatement {
+
+	public static void main(String[] args) {
+ 
+		  int intAge =18;
+		
+		IfElseControlStatement objIfElseControlStatement = new IfElseControlStatement();
+		objIfElseControlStatement.verifyArmyCandidateCutOffAge(intAge);
+		
+		objIfElseControlStatement.verifyAgeIsAdult();
+		
+		objIfElseControlStatement.verifyCandidateJoinArmyAgeLimitEligibility("25");
+		
+		objIfElseControlStatement.verifyArmyCandidateCutOffAge(30);
+		
+		objIfElseControlStatement.verifyCandidateVoterEligibility(18);
+		
+	}	
+	
+}		
+		
+		
+		
+		
+	
+
+
