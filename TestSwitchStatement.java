ed160aaef87c8eaccdcdb6e8f5fbdb3c0d1b4316
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-19 21:27:10 +0530

    Add files via upload

---
+++ TestSwitchStatement.java
@@ -0,0 +1,22 @@
+package com.vstl.javatraining;
+
+public class TestSwitchStatement {
+
+	public static void main(String[] args) {
+		
+		SwitchStatement objSwitchStatement=new SwitchStatement();
+		
+		  objSwitchStatement.daysInWeek(1);
+		  objSwitchStatement.daysInWeek(2);
+		  objSwitchStatement.daysInWeek(3);
+		  objSwitchStatement.daysInWeek(4);
+		  objSwitchStatement.daysInWeek(5);
+		  objSwitchStatement.daysInWeek(6);
+		  objSwitchStatement.daysInWeek(7);
+
+	}		  
+		  
+
+	}
+
+
